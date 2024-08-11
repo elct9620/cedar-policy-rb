fdf47f6eb264b909c0c76f333d54110f245c77d4
Aotokitsuruya <contact@aotoki.me>
2024-08-12 01:41:14 +0800

    Update ParseError binding to use new api

--- ext/cedar_policy/src/error.rs
+++ ext/cedar_policy/src/error.rs
@@ -1,10 +1,10 @@
-use magnus::{exception, value::Lazy, Error, ExceptionClass, Module, Ruby};
+use magnus::{value::Lazy, Error, ExceptionClass, Module, Ruby};
 
 use crate::CEDAR_POLICY;
 
 pub static PARSE_ERROR: Lazy<ExceptionClass> = Lazy::new(|ruby| {
     ruby.get_inner(&CEDAR_POLICY)
-        .define_error("ParseError", exception::standard_error())
+        .define_error("ParseError", ruby.exception_standard_error())
         .unwrap()
 });
 

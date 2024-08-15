e1bdf199db4f492a4573654b28083e18a794965f
Aotokitsuruya <contact@aotoki.me>
2024-08-16 01:36:48 +0800

    Add simplecov

--- Gemfile
+++ Gemfile
@@ -13,3 +13,5 @@ gem "rb_sys", "~> 0.9.63"
 gem "rspec", "~> 3.0"
 
 gem "rubocop", "~> 1.21"
+
+gem "simplecov", "~> 0.22.0"
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
+require "simplecov"
+SimpleCov.start do
+  add_filter "/spec/"
+  add_filter "/vendor/"
+end
+
 require "cedar_policy"
 
 RSpec.configure do |config|
